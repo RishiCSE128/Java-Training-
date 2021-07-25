1a9292795c732ddec973556676eb92e4fe7811a4
RishiCSE128 <saptarshi.ghosh.cs@gmail.com>
2021-07-25 22:05:27 +0100

    adds input feature to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your name... ");
+        String fname = sc.next();
+        String lname = sc.next();
+
+        System.out.println("Hello "+fname+" "+lname);
     }
 }
